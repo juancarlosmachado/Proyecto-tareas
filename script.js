0c76154314c1ebca1e2a33eff5c957e14d19b1ed
Juan Carlos Machado <juanchojs2801@gmail.com>
2022-11-09 10:29:51 -0300

    clase-04

--- script.js
+++ script.js
@@ -9,19 +9,29 @@ const createTask = (evento) => {
     task.classList.add("card");
     input.value = ' ';
     //backticks
-    const content =
-        `<div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-        </div>
+    console.log(checkComplete());
+    const taskContent = document.createElement('div');    
+    const titleTask = document.createElement('span');
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+    const content =`
         <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
-
+    task.appendChild(taskContent);
     list.appendChild(task);
 
     console.log(content);
 }
 
 console.log(btn);
-
 btn.addEventListener("click", (createTask));
+
+const checkComplete = () => {
+    const i = document.createElement('i');
+    i.classList.add("far");
+    i.classList.add("fa-check-square");
+    i.classList.add("icon");
+    return i;
+
+}
