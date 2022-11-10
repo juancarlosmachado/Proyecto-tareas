55cdfc4243bce01cd089aba5da183d254a22e4d4
Juan Carlos Machado <juanchojs2801@gmail.com>
2022-11-09 22:49:48 -0300

    import/export

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const btn = document.querySelector("[data-form-btn]");
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
+const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
@@ -9,29 +11,16 @@ const createTask = (evento) => {
     task.classList.add("card");
     input.value = ' ';
     //backticks
-    console.log(checkComplete());
     const taskContent = document.createElement('div');    
     const titleTask = document.createElement('span');
     titleTask.classList.add("task");
     titleTask.innerText = value;
     taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-    const content =`
-        <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
     task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
-
-    console.log(content);
 }
 
-console.log(btn);
 btn.addEventListener("click", (createTask));
-
-const checkComplete = () => {
-    const i = document.createElement('i');
-    i.classList.add("far");
-    i.classList.add("fa-check-square");
-    i.classList.add("icon");
-    return i;
-
-}
